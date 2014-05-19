929d5ba5ae6193f498faf525bc3a6c7c2b299e81
Colin Pringle-Wood <colin.pringlewood@gmail.com>
2014-05-19 16:36:08 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,4 @@
+'''within the settings file you will need to set the AUTH_USER_MODEL to your new custome user model that you have 
+  created within your models.py file just drop in this following line of code and change APP_NAME to the name of your app'''
+  
+AUTH_USER_MODEL = 'APP_NAME.MyUser'
